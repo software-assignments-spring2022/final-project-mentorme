3a6869255d2f764bb75587314bc285530cb354ae
jzlSniper <jiangzl.bruce@gmail.com>
2022-03-04 20:45:29 -0500

    local development set up

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Welcome to MentorMe. Let's start hacking!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
