0c05bfe572db530418ff026255ac0cb155668ac8
izzyjh <ijh234@nyu.edu>
2022-03-09 08:51:49 -0500

    minor changes

--- front-end/src/IndividualProfile.js
+++ front-end/src/IndividualProfile.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./styles/IndividualProfile.css"
-import image from './images/10 Individual Profile.png'
 import { Button } from "./components/Button";
 
 
@@ -10,7 +9,6 @@ const IndividualProfile = props => {
     <div className="IndividualProfile">
       <h1>Welcome!</h1>
       <section className="main-content">
-        <img className="image" alt="welcome!" src={image} />
         <p>
           Individual Profile
           <br />
