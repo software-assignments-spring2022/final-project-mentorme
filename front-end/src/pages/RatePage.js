45cb86b13d50fc298f4ae6b381e028c854303dd4
MateusSilvaAragao <aragaosm@icloud.com>
2022-03-21 12:13:03 -0400

    added content for post comment pa ge and rate mentor page

--- front-end/src/pages/RatePage.js
+++ front-end/src/pages/RatePage.js
@@ -3,21 +3,30 @@ import { Link } from "react-router-dom"
 import "../styles/RatePage.css"
 import image from '../images/11 Rate.png'
 import BurgerMenu from "../components/BurgerMenu"
-
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box'
 const RatePage = props => {
+  const [value, setValue] = React.useState(2);
+
   return (
     <div className="RatePage">
       <BurgerMenu/>
-      <h1>Welcome!</h1>
-      <section className="main-content">
-        <img className="image" alt="welcome!" src={image} />
-        <p>
-          Rate Mentor Page
-          <br />
-          <br />
-          <Link to="/mentorMe"><button>Back to mentorMe home!</button></Link>
-        </p>
-      </section>
+      <img 
+        alt="welcome!"
+        src={image} className="center"
+      />
+     <Stack spacing={1}direction="column"
+  alignItems="center"
+  justifyContent="center"  style={{ minHeight: '50vh' }}>
+    <h2 >Current Rate</h2>
+    <Rating name="read-only" value={value} readOnly size="large"/>
+    <h2>Your Rate</h2>
+
+    <Rating name="size-large" defaultValue={2} size="large" />
+    </Stack>
+
     </div>
   )
 }
