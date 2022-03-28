2c4ead63e455c336055af6b012825da29f06f3a5
xuyou1999 <yx1711@nyu.edu>
2022-03-28 00:50:23 -0400

    basic search back-end

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,15 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 // we will put some server logic here later...
+app.use(morgan("dev"))
+
+// Send back search results on advisor search page
+app.get("/rateAdvisor/searchResult", (req, res) => {
+    console.log(req.query)
+    res.send("Goodbye world!")
+  })
+
 // export the express app we created to make it available to other modules
 module.exports = app
