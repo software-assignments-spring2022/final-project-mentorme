0640cc3a149243098b326bbdf5e198bee1e7f0ff
izzyjh <ijh234@nyu.edu>
2022-04-01 00:07:57 -0400

    Added the basics

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -7,6 +7,16 @@ const stringSimilarity = require("string-similarity");
 // we will put some server logic here later...
 app.use(morgan("dev"))
 
+
+
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+// make 'public' directory publicly readable with static content
+app.use("/static", express.static("public"))
+
+
+
 // handle search get request on advisor page. Return the most relevant 3 results. Req should has search term as 'name'
 function calulateRank (term, arr) {
     let sim_arr = []
@@ -30,6 +40,40 @@ function calulateRank (term, arr) {
     }
     return output_arr
 }
+
+app.get("/", (req, res) => {
+    res.send("Home")
+})
+
+app.get("/mentorMe" , (req, res) => {
+    res.send("MentorMe Home")
+})
+
+app.get("/mentorMe/UserProfile", (req, res) => {
+    const userinfo = {
+        name: "UserName",
+        bio: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in auctor justo, id tristique nisi. Sed at massa risus. Nunc imperdiet vehicula sapien, a molestie orci aliquam molestie. Aenean non leo in velit venenatis blandit. Aliquam eu sapien nec n",
+        profilepic: "https://picsum.photos/200/300/",
+    }
+    res.json(userinfo)
+})
+
+app.get("/mentorMe/UserProfile/EditProfile", (req, res) => {
+    const userinfo = {
+        name: "UserName",
+        bio: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in auctor justo, id tristique nisi. Sed at massa risus. Nunc imperdiet vehicula sapien, a molestie orci aliquam molestie. Aenean non leo in velit venenatis blandit. Aliquam eu sapien nec n",
+        profilepic: "https://picsum.photos/200/300/",
+        email: "UserName@gmail.com",
+        password: "thePassword123",
+    }
+    res.json(userinfo)
+})
+
+app.get("/mentorMe/profileDisplay/individualProfile" , (req, res) => {
+    res.send("Individual Profile")
+})
+
+
 
 app.get("/rateAdvisor/searchResult", (req, res) => {
     console.log(req.query)
