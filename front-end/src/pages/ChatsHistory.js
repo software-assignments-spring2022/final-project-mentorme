d34f097af0c020c15d4ae5bf05417212d5cfe5a5
Lijie Xie <doomarktse@gmail.com>
2022-04-03 16:55:36 -0400

    implement chat back-end

--- front-end/src/pages/ChatsHistory.js
+++ front-end/src/pages/ChatsHistory.js
@@ -10,107 +10,107 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const ChatsHistory = props => {
   return (
     <div className="ChatsHistory">
-      <BurgerMenu/>ChatsHistory
+      <BurgerMenu />ChatsHistory
       <section className="main-content">
-        <img className="image" alt="welcome!" src={image} />
-      
+        {/* <img className="image" alt="welcome!" src={image} /> */}
+
         <div className="search">
           <SearchBar label='Search a Private Chat' navigateTo='/mentorMe/profileDisplay/individualProfile/individualChat/' />
           <div className='clear' />
         </div>
         <section className="resultList">
-        <Container className="">
+          <Container className="">
 
-          <div className="list-group">
+            <div className="list-group">
 
-            {/* Data 1 */}
-            <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/201" }} className="list-group-item list-group-item-action" aria-current="">
-              <Container className="container-search">
-                <Row className="rows">
-                  <Col xs={4} md={2} className="columns">
-                    <div className="advisorPic">
-                      <img className="advisor-picture" src="https://picsum.photos/201" alt="picture" />
-                    </div>
-                  </Col>
-                  <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">Mentor Name</h5>
-                    <small className="text-muted"> seen 3 min ago</small>
-                  </div >
-                    <div className="school-name">                    
-                     <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </Link>
+              {/* Data 1 */}
+              <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/201" }} className="list-group-item list-group-item-action" aria-current="">
+                <Container className="container-search">
+                  <Row className="rows">
+                    <Col xs={4} md={2} className="columns">
+                      <div className="advisorPic">
+                        <img className="advisor-picture" src="https://picsum.photos/201" alt="picture" />
+                      </div>
+                    </Col>
+                    <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">Mentor Name</h5>
+                      <small className="text-muted"> seen 3 min ago</small>
+                    </div >
+                      <div className="school-name">
+                        <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Link>
 
-            {/* Data 2 */}
+              {/* Data 2 */}
 
-            <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/202" }} className="list-group-item list-group-item-action" aria-current="">
-              <Container className="container-search">
-                <Row className="rows">
-                  <Col xs={4} md={2} className="columns">
-                    <div className="advisorPic">
-                      <img className="advisor-picture" src="https://picsum.photos/202" alt="picture" />
-                    </div>
-                  </Col>
-                  <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">Mentor Name</h5>
-                    <small className="text-muted"> online</small>
-                  </div >
-                    <div className="school-name">                    
-                     <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </Link>
-{/* Data 3 */}
-<Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/203" }} className="list-group-item list-group-item-action" aria-current="">
-              <Container className="container-search">
-                <Row className="rows">
-                  <Col xs={4} md={2} className="columns">
-                    <div className="advisorPic">
-                      <img className="advisor-picture" src="https://picsum.photos/203" alt="picture" />
-                    </div>
-                  </Col>
-                  <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">Mentor Name</h5>
-                    <small className="text-muted"> Delivered</small>
-                  </div >
-                    <div className="school-name">                    
-                     <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </Link>
-  {/* Data 4 */}
-  <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/204" }} className="list-group-item list-group-item-action" aria-current="">
-              <Container className="container-search">
-                <Row className="rows">
-                  <Col xs={4} md={2} className="columns">
-                    <div className="advisorPic">
-                      <img className="advisor-picture" src="https://picsum.photos/204" alt="picture" />
-                    </div>
-                  </Col>
-                  <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">Mentor Name</h5>
-                    <small className="text-muted"> Delivered</small>
-                  </div >
-                    <div className="school-name">                    
-                     <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </Link>
+              <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/202" }} className="list-group-item list-group-item-action" aria-current="">
+                <Container className="container-search">
+                  <Row className="rows">
+                    <Col xs={4} md={2} className="columns">
+                      <div className="advisorPic">
+                        <img className="advisor-picture" src="https://picsum.photos/202" alt="picture" />
+                      </div>
+                    </Col>
+                    <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">Mentor Name</h5>
+                      <small className="text-muted"> online</small>
+                    </div >
+                      <div className="school-name">
+                        <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Link>
+              {/* Data 3 */}
+              <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/203" }} className="list-group-item list-group-item-action" aria-current="">
+                <Container className="container-search">
+                  <Row className="rows">
+                    <Col xs={4} md={2} className="columns">
+                      <div className="advisorPic">
+                        <img className="advisor-picture" src="https://picsum.photos/203" alt="picture" />
+                      </div>
+                    </Col>
+                    <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">Mentor Name</h5>
+                      <small className="text-muted"> Delivered</small>
+                    </div >
+                      <div className="school-name">
+                        <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Link>
+              {/* Data 4 */}
+              <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/" state={{ imgSrc: "https://picsum.photos/204" }} className="list-group-item list-group-item-action" aria-current="">
+                <Container className="container-search">
+                  <Row className="rows">
+                    <Col xs={4} md={2} className="columns">
+                      <div className="advisorPic">
+                        <img className="advisor-picture" src="https://picsum.photos/204" alt="picture" />
+                      </div>
+                    </Col>
+                    <Col xs={8} md={10} className="columns"> <div className="d-flex w-100 justify-content-between">
+                      <h5 className="mb-1">Mentor Name</h5>
+                      <small className="text-muted"> Delivered</small>
+                    </div >
+                      <div className="school-name">
+                        <small > Lorem ipsum dolor sit amet, consectetur adipiscing elit </small>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Link>
 
-          </div>
+            </div>
 
-        </Container>
-      </section>
-    
+          </Container>
+        </section>
+
       </section>
     </div>
   )
--- front-end/src/pages/IndividualChat.js
+++ front-end/src/pages/IndividualChat.js
@@ -6,62 +6,107 @@ import { Button } from "../components/Button";
 import BurgerMenu from "../components/BurgerMenu";
 import Box from '@mui/material/Box';
 import { useEffect, useRef, useState } from 'react';
+import io from 'socket.io-client'
+
+
+const socket = io.connect("http://localhost:3001");
+// const messageContainer = document.getElementById("message-container")
+// const messageForm = document.getElementById('send-container')
+// const messageInput = document.getElementById('message-input')
+// function appendMessage(message) {
+//   const messageElement = React.createElement("h1", {}, "Hello World");
+//   messageElement.innerText = message;
+//   messageContainer.append(messageElement)
+// }
+
 const IndividualChat = props => {
   const [messages, setMessages] = useState([]);
-
+  const [username, setUsername] = useState("");
+  const chatBox = useRef();
   // Binding
   const messageText = useRef();
 
   // Side Effects / Lifecycle
   useEffect(() => {
     const existingMessage = localStorage.getItem('message');
+    // setMessages(existingMessage ? JSON.parse(existingMessage) : []);
     setMessages(existingMessage ? JSON.parse(existingMessage) : []);
   }, []);
 
   // Events
   function addMessage(event) {
     event.preventDefault();
-    const next = [...messages, messageText.current.value];
+
+  }
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const message = "YOU: " + messageText.current.value;
+    const next = [...messages, message];
     setMessages(next);
     localStorage.setItem('todos', JSON.stringify(next));
+    socket.emit('send-chat-message', messageText.current.value);
+    messageText.current.value = '';
+
   }
+  socket.on("chat-message", data => {
+    const incoming = "THE OTHER USER: " + data;
+    const nextMes = [...messages, incoming];
+    setMessages(nextMes);
+    localStorage.setItem('todos', JSON.stringify(nextMes));
+    data = '';
+  })
+
   return (
     <div className="IndividualChat">
-      
-      <BurgerMenu/>
-      
-    
-      <section className="main-content">
-        <img className="image" alt="welcome!" src={image} />
-        <p>
-          Mentor Name
-          <br />
-          <br />
-          <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/ratePage"><Button buttonStyle={"btn--danger--solid"} buttonSize={'btn--medium--long'}>End Chat and Rate</Button></Link>
-          
-        </p>
-        <Box
-        
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      p: 1,
-      m: 1,
-      bgcolor: 'background.paper',
-      borderRadius: 5,
-    }}
-  ><div className='chat'>
-      <ul>
-        {messages.map(message => (<li key={message}>{message}</li>))}  
-      </ul>
-      <form onSubmit={addMessage}>
-        <input type="text" placeholder="Enter Message" ref={messageText} />
-        <input type="submit" value="Send" />
-      </form>
 
-    </div>
-     
-  </Box>
+
+      <BurgerMenu />
+
+      <Box id="message-container"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.paper',
+          borderRadius: 5,
+        }}
+      >
+        <div className='chat'>
+          <ul>
+
+            {messages.map(message => (<li key={message}>{message}</li>))}
+          </ul>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter Message" ref={messageText} />
+            <input type="submit" value="Submit" />
+          </form>
+          <br></br>
+          <Link to="/mentorMe/profileDisplay/individualProfile/individualChat/ratePage"><Button className="to-rate" buttonStyle={"btn--danger--solid"} buttonSize={'btn--medium--long'} >End Chat and Rate</Button></Link>
+
+
+        </div>
+
+        {/* socket.io test */}
+        {/* <div id="message-container"></div> */}
+        {/* <form onSubmit={handleSubmit} id="send-container">
+          <input type="text" id="message-input" />
+          <button type="submit" id="send-button">Send</button>
+        </form> */}
+
+
+      </Box>
+
+
+      <section className="main-content">
+        {/* <img className="image" alt="welcome!" src={image} /> */}
+        <p>
+          {/* Mentor Name */}
+          <br />
+          <br />
+        </p>
       </section>
     </div>
   )
