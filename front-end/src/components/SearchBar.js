dbdc42b66f2da6b0b1fe4a3ead56cd063a73cf34
jzlSniper <jiangzl.bruce@gmail.com>
2022-04-04 02:48:41 -0700

    bug fixed

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -10,50 +10,10 @@ import Suggestion from "./Suggestion";
   and 'isMentorMe' specifies the search bar is used in MentorMe or RateMyAdvisor*/
   
 const SearchBar = (props) => {
-
-  // mock data
-  // if fetching from mockaroo fails because of free account limit,
-  // this mock data will be used
-  const mockData = [{
-    "id": 1,
-    "first_name": "Mayer",
-    "last_name": "Ballentime",
-    "school": "Skynoodle",
-    "department": "Human Resources",
-    "score": 0.7
-  }, {
-    "id": 2,
-    "first_name": "Margo",
-    "last_name": "Vigietti",
-    "school": "Dabtype",
-    "department": "Engineering",
-    "score": 1.2
-  }, {
-    "id": 3,
-    "first_name": "Daveta",
-    "last_name": "Ochterlony",
-    "school": "Izio",
-    "department": "Legal",
-    "score": 3.4
-  }, {
-    "id": 4,
-    "first_name": "Elyssa",
-    "last_name": "Ayling",
-    "school": "Edgeclub",
-    "department": "Accounting",
-    "score": 2.5
-  }, {
-    "id": 5,
-    "first_name": "Joela",
-    "last_name": "Thing",
-    "school": "Kazu",
-    "department": "Legal",
-    "score": 2.8
-  }]
   
   const navigate = useNavigate();
   const [name, setName] = useState("");
-  const [suggestion, setSuggestion] = useState(mockData)
+  const [suggestion, setSuggestion] = useState([])
   const handleSubmit = (event) => {
     event.preventDefault();
     navigate(props.navigateTo, {state:{id: 300, name: name}})
@@ -61,12 +21,12 @@ const SearchBar = (props) => {
 
   {/* get suggestions from backend */}
   useEffect(async () => {
-    await axios.get("https://api.mockaroo.com/api/4b0b0f90?count=10&key=de852b30")
+    await axios.get("http://localhost:4000/rateAdvisor/searchResult/", { params: { name: name } })
       .then(res => {
         setSuggestion(res.data);
       })
       .catch(err => {
-        setSuggestion(mockData);
+        console.log("cannot get backend suggestions. err.")
       })
   }, [name])
 
