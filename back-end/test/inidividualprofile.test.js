b29f0721fcd39e5ae0e3971ee8083cbaba5c6bbe
jzlSniper <jiangzl.bruce@gmail.com>
2022-04-06 13:31:34 -0400

    tests fixed

--- back-end/test/inidividualprofile.test.js
+++ back-end/test/inidividualprofile.test.js
@@ -10,14 +10,14 @@ describe("GET request to /mentorMe/profileDisplay/individualProfile route", () =
     it("it should respond with an HTTP 200 status code and an object in the response body \n the response body object should contain three different properties: a username, bio, and profilePic", done => {
       chai
         .request(server)
-        .get("/mentorMe/profileDisplay/individualProfile")
+        .get("/mentorMe/profileDisplay/individualProfile/1")
         .end((err, res) => {
           res.should.have.status(200) // use should to make BDD-style assertions
           res.body.should.be.a("object") // our route sends back an object
-          res.body.should.have.property("username") // a way to check the exact value of a property of the response object
+          res.body.should.have.property("last_name") // a way to check the exact value of a property of the response object
           res.body.should.have.property("bio")
           res.body.should.have.property("profilePic")
-          res.body.username.should.be.a("string")
+          res.body.last_name.should.be.a("string")
           res.body.bio.should.be.a("string")
           res.body.profilePic.should.be.a("string") //for right now while using api instead of database
           done() // resolve the Promise that these tests create so mocha can move on
