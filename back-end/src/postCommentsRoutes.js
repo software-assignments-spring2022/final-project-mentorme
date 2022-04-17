7fe5698dfad08c2edad00c40ccadd80610412b83
MateusSilvaAragao <aragaosm@icloud.com>
2022-04-17 19:47:24 -0400

    commiting some changes to pull

--- back-end/src/postCommentsRoutes.js
+++ back-end/src/postCommentsRoutes.js
@@ -8,26 +8,28 @@ const {Comments} =require("./models/Comments")
 
 router.post("/:id", async(request, res) => {
     const userId = request.params.id
+    // const nameData = new Name({firstName:firstName, lastName:lastName});
+    // nameData.save().then(res.redirect('/')).catch(err){
+    // console.log(err);
+    // };
     console.log(userId)
-    try {
-        const user = await Comments.find({id:userId})
-        const data = {
-                    overall: request.body.overall,
-                    category1: request.body.category1,
-                    category2: request.body.category2,
-                    category3: request.body.category3,
-                    category4:request.body.category4,
-                    comment:request.body.comments,
-            }
-            console.log(request.body);
-            res.json(data)
-        console.log(user)
-        console.log("found user")
-    } catch (e) {
-        console.log("Couldn't Find User");
-        res.status(500)
-    }
-    
-})
+    const data = {
+        overall: request.body.overall,
+        category1: request.body.category1,
+        category2: request.body.category2,
+        category3: request.body.category3,
+        category4:request.body.category4,
+        comment:request.body.comments,
+    };
+    const newComment = new Comments({id:userId,score:data.overall,
+                                    category1:data.category1,
+                                    category2:data.category2,
+                                    category3:data.category3,
+                                    category4:data.category4,
+                                    written_feedback:data.comment});
+    newComment.save().then(res.redirect('/'))
+        .catch(err => {});
+    console.log(comments)
+});
 
 module.exports = router;
