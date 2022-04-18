4497c2d8fd0e4bc1c9f626316e83610041e4c5a9
izzyjh <ijh234@nyu.edu>
2022-04-18 01:36:56 -0400

    minor changes

--- back-end/src/editprofile.js
+++ back-end/src/editprofile.js
@@ -10,12 +10,9 @@ const storage = multer.diskStorage({
       cb(null, "/uploads")
     },
     filename: function (req, file, cb) {
-      // take apart the uploaded file's name so we can create a new one based on it
       const extension = path.extname(file.originalname)
       const basenameWithoutExtension = path.basename(file.originalname, extension)
-      // create a new filename with a timestamp in the middle
       const newName = `${basenameWithoutExtension}-${Date.now()}${extension}`
-      // tell multer to use this new filename for the uploaded file
       cb(null, newName)
     },
 })
@@ -29,10 +26,9 @@ app.use('/uploads', express.static('uploads'));
 router.post("/:id", upload.array('profilePic'), async (req, res, next) => {
 
     const userId = req.params.id
-    console.log(req.body.profilePic.path)
 
     try {
-        const userinfo = await User.findOneAndUpdate({ id : userId }, { first_name : req.body.username, email : req.body.email, bio : req.body.bio, password : req.body.password, profilePic : req.body.profilePic.path})
+        const userinfo = await User.findOneAndUpdate({ id : userId }, { first_name : req.body.username, email : req.body.email, bio : req.body.bio, password : req.body.password, profilePic : req.body.profilePic.newName})
         res.json(userinfo) 
     } catch (e) {
         console.log("Couldn't Find User");
